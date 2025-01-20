dc06c1b4edc89995d197dd1f230e746adc212cbd
Adriaan Joubert <45142747+adriaanjoubert@users.noreply.github.com>
2025-01-20 20:05:28 +0200

    Add unit test with mocks and asserts

--- tests.py
+++ tests.py
@@ -1,10 +1,70 @@
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, call
 
 from main import Robot, Command
 
 
 class RobotTestCase(TestCase):
+
+    @patch("main.input")
+    def test_collect_command(self, mock_input: MagicMock) -> None:
+        robot = Robot()
+        mock_input.side_effect = ["1", "1,2"]
+        command = robot._collect_command()
+        self.assertEqual(command.operation, "move")
+        self.assertEqual(command.kwargs["destination"], (1.0, 2.0))
+
+    @patch("main.Robot._execute")
+    @patch("main.Robot._collect_command")
+    @patch("main.input")
+    def test_run(
+        self,
+        mock_input: MagicMock,
+        mock_collect_command: MagicMock,
+        mock_execute: MagicMock,
+    ) -> None:
+        mock_input.side_effect = ["2", "2", "1", "1", "1", "3", "4"]
+        command_1 = Command(
+            kwargs={"object_location": (0.0, 0.0)},
+            operation="lift",
+        )
+        command_2 = Command(
+            kwargs={"message": "Hello, world!"},
+            operation="speak",
+        )
+        command_3 = Command(
+            kwargs={"destination": (1.0, 0.0)},
+            operation="move",
+        )
+        command_4 = Command(
+            kwargs={"destination": (1.0, 1.0)},
+            operation="move",
+        )
+        command_5 = Command(
+            kwargs={"destination": (0.0, 1.0)},
+            operation="move",
+        )
+        mock_collect_command.side_effect = [
+            command_1,
+            command_2,
+            command_3,
+            command_4,
+            command_5,
+        ]
+        robot = Robot()
+        robot.run()
+        mock_execute.assert_has_calls(
+            [
+                call(command=command_1),
+                call(command=command_2),
+                call(command=command_3),
+                call(command=command_4),
+                call(command=command_5),
+            ]
+        )
+
+
+class RobotIntegrationTestCase(TestCase):
 
     @patch("main.input")
     def test_run(self, mock_input: MagicMock) -> None:
